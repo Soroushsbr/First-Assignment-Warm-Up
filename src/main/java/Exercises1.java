5e594bba35bf794fbcae38bde0d9cac8bb58d5b3
Soroush Saberi <soroush.13830o@gmail.com>
2024-02-25 20:14:39 +0330

    Final

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,42 +1,42 @@
 public class Exercises1 {
-    /*
-    implement a function that returns factorial of given n
-     */
-    public long factorial(int n) {
-        // TODO
-        return 0;
+
+    public static long factorial(int n) {
+        int sum = 1;
+        for(int i = 1 ; i <= n ; i++)
+            sum *= i;
+        return sum;
     }
 
-
-    /*
-    implement a function that return nth number of fibonacci series
-    the series -> 1, 1, 2, 3, 5, 8, ...
-    */
-    public long fibonacci(int n) {
-        // TODO
-        return 0;
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
 
-
-    /*
-    implement a function that returns a triangle for example:
-    row = 5
-
-    *
-    **
-    ***
-    ****
-    *****
-
-     */
-    public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+    public static char[][] generateTriangle(int rows) {
+        char[][] triangle = new char[rows][];
+        for(int i = 0 ; i < rows; i++){
+            triangle[i] = new char[i + 1];
+            for(int j = 0 ; j <= i ; j++){
+                triangle[i][j] = '*';
+            }
+        }
+        return triangle;
     }
 
 
     public static void main(String[] args) {
-        // test your code here!
+       System.out.println(factorial(5));
+       System.out.println(fibonacci(6));
+       char[][] tri = generateTriangle(5);
+       for (int i = 0; i < 5; i++) {
+           for (int j = 0; j <= i; j++) {
+               System.out.print(tri[i][j]);
+           }
+           System.out.println();
+       }
     }
 
 }
